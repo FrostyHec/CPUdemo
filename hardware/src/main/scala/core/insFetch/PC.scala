ccd94792bf3147b0ad9b2d4ebad6a8228b105614
Frosty <112929809+FrostyHec@users.noreply.github.com>
2024-04-26 22:09:44 +0800

    已完成目前能完成的部分
    Signed-off-by: Frosty <112929809+FrostyHec@users.noreply.github.com>

--- hardware/src/main/scala/core/insFetch/PC.scala
+++ hardware/src/main/scala/core/insFetch/PC.scala
@@ -9,10 +9,26 @@ class PC extends Module{
   val io=IO(new Bundle {
     val cpu_state=Input(CPUStateType.getWidth)
 
-    val nextAddr =Input(UInt(32.W))
+    val next_addr =Input(UInt(32.W))
 
     val addr=Output(UInt(32.W))
   })
-  io.addr:=io.nextAddr
-  //TODO  PC
+  val pc=RegInit(0.U(32.W))
+  io.addr:=pc
+  when(io.cpu_state===CPUStateType.sWritePC.getUInt){
+    pc:=io.next_addr
+  }.otherwise{
+    //do nothing
+  }
+}
+object PC extends App {//name had better to be same as class name, put under the class file
+  // These lines generate the Verilog output
+  println(
+    new(chisel3.stage.ChiselStage).emitVerilog(
+      new PC(),//use your module class
+      Array(
+        "--target-dir", "generated_dut/"
+      )
+    )
+  )
 }
