4399820c499b54e8c735c6808280a5965876cea6
redhecker <12210305@mail.sustech.edu.cn>
2024-04-29 23:59:10 +0800

    finish immGen, next PC Gen and Opselector with test cases

--- hardware/src/main/scala/core/execute/ImmGen.scala
+++ hardware/src/main/scala/core/execute/ImmGen.scala
@@ -1,13 +1,56 @@
 package core.execute
-import chisel3._
-import core.config._
-class ImmGen extends Module{
-  val io=IO(new Bundle{
-    val raw_imm=Input(UInt(32.W))
-    val unsigned=Input(Bool())
-    val imm_width=Input(ImmWidthType.getWidth)
 
-    val real_imm=Output(UInt(32.W))
+import chisel3._
+import chisel3.util._
+import core.config._
+
+class ImmGen extends Module{
+  val io = IO(new Bundle{
+    val raw_imm: UInt = Input(UInt(32.W))
+    val unsigned: Bool = Input(Bool())
+    val imm_width: UInt = Input(ImmWidthType.getWidth)
+
+    val real_imm: UInt = Output(UInt(32.W))
   })
-  //TODO imm gen
+
+  io.real_imm := DontCare
+  switch(io.imm_width){
+    is(ImmWidthType.Eleven.getUInt){
+      when(io.unsigned){
+        io.real_imm := Cat(Fill(21, 0.U), io.raw_imm(11, 0))
+      }.otherwise{
+        io.real_imm := Cat(Fill(21, io.raw_imm(11)), io.raw_imm(11, 0))
+      }
+    }
+    is(ImmWidthType.Twelve.getUInt){
+      when(io.unsigned){
+        io.real_imm := Cat(Fill(19, 0.U), io.raw_imm(11), io.raw_imm(0), io.raw_imm(10, 1), 0.U)
+      }.otherwise{
+        io.real_imm := Cat(Fill(20, io.raw_imm(11)), io.raw_imm(0), io.raw_imm(10, 1), 0.U)
+      }
+    }
+    is(ImmWidthType.Twenty.getUInt){
+      when(io.unsigned){
+        io.real_imm := Cat(Fill(11, 0.U), io.raw_imm(19), io.raw_imm(7, 0), io.raw_imm(8) , io.raw_imm(18, 9), 0.U)
+      }.otherwise{
+        io.real_imm := Cat(Fill(12, io.raw_imm(19)), io.raw_imm(7, 0), io.raw_imm(8) , io.raw_imm(18, 9), 0.U)
+      }
+    }
+    is(ImmWidthType.ThirtyOne.getUInt){
+      io.real_imm := Cat(io.raw_imm(19, 0), Fill(12, 0.U))
+    }
+  }
 }
+
+object ImmGen extends App {//name had better to be same as class name, put under the class file
+  // These lines generate the Verilog output
+  println(
+    new(chisel3.stage.ChiselStage).emitVerilog(
+      new ImmGen(),//use your module class
+      Array(
+        "--target-dir", "generated_dut/"
+      )
+    )
+  )
+}
+
--- hardware/src/main/scala/core/execute/OperandSelector.scala
+++ hardware/src/main/scala/core/execute/OperandSelector.scala
@@ -1,16 +1,39 @@
 package core.execute
 
 import chisel3._
+import chisel3.util._
 import core.config._
+
 class OperandSelector extends Module {
   val io=IO(new Bundle{
-    val rs1_val=Input(UInt(32.W))
-    val rs2_val=Input(UInt(32.W))
-    val real_imm=Input(UInt(32.W))
-    val operand2Type=Input(Operand2Type.getWidth)
+    val rs1_val: UInt = Input(UInt(32.W))
+    val rs2_val: UInt = Input(UInt(32.W))
+    val real_imm: UInt = Input(UInt(32.W))
+    val operand2Type: UInt = Input(Operand2Type.getWidth)
 
-    val operand1=Output(UInt(32.W))
-    val operand2=Output(UInt(32.W))
+    val operand1: UInt = Output(UInt(32.W))
+    val operand2: UInt = Output(UInt(32.W))
   })
-  //TODO op selector
+  io.operand1 := io.rs1_val
+  io.operand2 := DontCare
+  switch(io.operand2Type){
+    is(Operand2Type.Imm.getUInt){
+      io.operand2 := io.real_imm
+    }
+    is(Operand2Type.Reg2.getUInt){
+      io.operand2 := io.rs2_val
+    }
+  }
+}
+
+object OperandSelector extends App {//name had better to be same as class name, put under the class file
+  // These lines generate the Verilog output
+  println(
+    new(chisel3.stage.ChiselStage).emitVerilog(
+      new OperandSelector(),//use your module class
+      Array(
+        "--target-dir", "generated_dut/"
+      )
+    )
+  )
 }
