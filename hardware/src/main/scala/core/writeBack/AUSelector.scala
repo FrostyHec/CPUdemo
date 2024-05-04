25d537139ccbcaec7b5cbbdc104216dd1739fae3
Victor0700 <1962075177@qq.com>
2024-05-05 00:33:13 +0800

    ControlUnit AUSelector WriteDataSelector

--- hardware/src/main/scala/core/writeBack/AUSelector.scala
+++ hardware/src/main/scala/core/writeBack/AUSelector.scala
@@ -1,6 +1,7 @@
 package core.writeBack
 import chisel3._
 import core.config._
+import chisel3.util._
 class AUSelector extends Module{
   val io=IO(new Bundle{
     val au_type=Input(AUType.getWidth)
@@ -10,5 +11,27 @@ class AUSelector extends Module{
 
     val au_out=Output(UInt(32.W))
   })
-  //TODO AU selector
+
+  io.au_out := DontCare
+
+  switch(io.au_type) {
+    is(AUType.ALU.getUInt) {
+      io.au_out := io.alu_result
+    }
+    is(AUType.CMP.getUInt) {
+      io.au_out := io.cmp_result
+    }
+  }
+}
+
+object AUSelector extends App {//name had better to be same as class name, put under the class file
+  // These lines generate the Verilog output
+  println(
+    new(chisel3.stage.ChiselStage).emitVerilog(
+      new AUSelector(),//use your module class
+      Array(
+        "--target-dir", "generated_dut/"
+      )
+    )
+  )
 }
