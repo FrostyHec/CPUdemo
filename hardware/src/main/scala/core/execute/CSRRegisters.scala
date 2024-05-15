377875c0d10d7ef9b20b60eeaabaf19e4e13dec0
Frosty <112929809+FrostyHec@users.noreply.github.com>
2024-05-15 16:38:30 +0800

    Signed-off-by: Frosty <112929809+FrostyHec@users.noreply.github.com>

--- hardware/src/main/scala/core/execute/CSRRegisters.scala
+++ hardware/src/main/scala/core/execute/CSRRegisters.scala
@@ -3,6 +3,6 @@ package core.execute
 import chisel3._
 
 class CSRRegisters extends Module {
-
   //TODO CSR Registers
+  // CSR REGISTERS SHOULD DO THE FOLLOWING MODIFICATION: SYN_READ FROM REGS, MEM and INS MEM
 }
