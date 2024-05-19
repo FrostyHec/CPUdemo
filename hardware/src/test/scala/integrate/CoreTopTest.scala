704992ddebd6d0f6a59bb9658409a1c5f57ee02f
redhecker <12210305@mail.sustech.edu.cn>
2024-05-19 23:40:50 +0800

    add coreTop test

--- hardware/src/test/scala/integrate/CoreTopTest.scala
+++ hardware/src/test/scala/integrate/CoreTopTest.scala
@@ -45,12 +45,12 @@ class CoreTopTest extends FlatSpec with ChiselScalatestTester with Matchers {
     }
   }
   it should "not beq" in {
-    //            addi x1,x0,1
-    //            addi x2,x1,1
-    //            beq x2,x1,NEXT
-    //            addi x3,x0,1
-    //            addi x4,x0,1
-    //            addi x5,x0,1
+    //      addi x1,x0,1
+    //      addi x2,x1,1
+    //      beq x2,x1,NEXT
+    //      addi x3,x0,1
+    //      addi x4,x0,1
+    //      addi x5,x0,1
     //      NEXT:
     //            addi x3,x0,10
     //            -> x3=10,x4=1,x5=1
@@ -66,5 +66,190 @@ class CoreTopTest extends FlatSpec with ChiselScalatestTester with Matchers {
   // lb,sb,lw,sw
   // bgeu,blt
   // jalr,jal,lui,auipc
+  it should "lw and sw" in {
+    //    addi x1, x0, 108
+    //    sw x1, (x2)
+    //    add x1, x0, x0
+    //    lw x1, (x2)
+    // todo data out 出错
+    load_instructions("lsw1.txt")
+    test(new CoreTop) { cpu =>
+      run_instructions(cpu, 4)
+      checkRegsInCPU(cpu, 1, 108.U)
+    }
+  }
+
+  it should "lb and sb" in {
+    //    addi x1, x0, 108
+    //    sb x1, (x2)
+    //    add x1, x0, x0
+    //    lb x1, (x2)
+    // todo data out 出错
+    load_instructions("lsb1.txt")
+    test(new CoreTop) { cpu =>
+      run_instructions(cpu, 4)
+      checkRegsInCPU(cpu, 1, 108.U)
+    }
+  }
+
+  it should "not bgeu" in {
+    //      addi x1, x0, 1
+    //      addi x2, x0, 2
+    //      bgeu x1, x2, NEXT
+    //      addi x3, x0, 1
+    //      addi x4, x0, 1
+    //      addi x5, x0, 1
+    //      NEXT:
+    //      addi x3, x0, 10
+    //    -> x3=10,x4=1,x5=1
+    load_instructions("bgeu1.txt")
+    test(new CoreTop) { cpu =>
+      run_instructions(cpu, 7)
+      checkRegsInCPU(cpu, 3, 10.U)
+      checkRegsInCPU(cpu, 4, 1.U)
+      checkRegsInCPU(cpu, 5, 1.U)
+    }
+  }
+
+  it should "bgeu" in {
+    //      addi x1, x0, 2
+    //      addi x2, x0, 1
+    //      bgeu x1, x2, NEXT
+    //      addi x3, x0, 1
+    //      addi x4, x0, 1
+    //      addi x5, x0, 1
+    //      NEXT:
+    //      addi x3, x0, 10
+    //    -> x3=10,x4=0,x5=0
+    load_instructions("bgeu2.txt")
+    test(new CoreTop) { cpu =>
+      run_instructions(cpu, 4)
+      checkRegsInCPU(cpu, 3, 10.U)
+      checkRegsInCPU(cpu, 4, 0.U)
+      checkRegsInCPU(cpu, 5, 0.U)
+    }
+  }
+
+  it should "blt" in {
+    //      addi x1, x0, 1
+    //      addi x2, x0, 2
+    //      blt x1, x2, NEXT
+    //      addi x3, x0, 1
+    //      addi x4, x0, 1
+    //      addi x5, x0, 1
+    //      NEXT:
+    //      addi x3, x0, 10
+    //    -> x3=10,x4=0,x5=0
+    load_instructions("blt1.txt")
+    test(new CoreTop) { cpu =>
+      run_instructions(cpu, 4)
+      checkRegsInCPU(cpu, 3, 10.U)
+      checkRegsInCPU(cpu, 4, 0.U)
+      checkRegsInCPU(cpu, 5, 0.U)
+    }
+  }
+
+  it should "not blt" in {
+    //      addi x1, x0, 2
+    //      addi x2, x0, 1
+    //      blt x1, x2, NEXT
+    //      addi x3, x0, 1
+    //      addi x4, x0, 1
+    //      addi x5, x0, 1
+    //      NEXT:
+    //      addi x3, x0, 10
+    //    -> x3=10,x4=1,x5=1
+    load_instructions("blt2.txt")
+    test(new CoreTop) { cpu =>
+      run_instructions(cpu, 7)
+      checkRegsInCPU(cpu, 3, 10.U)
+      checkRegsInCPU(cpu, 4, 1.U)
+      checkRegsInCPU(cpu, 5, 1.U)
+    }
+  }
+
+  it should "jalr" in {
+    //      addi x1, x0, 8
+    //      jalr x2, x1, 4
+    //      addi x3, x0, 1
+    //      addi x4, x0, 1
+    //      addi x5, x0, 1
+    //    -> x2=8,x3=0,x4=1,x5=1
+    load_instructions("jalr1.txt")
+    test(new CoreTop) { cpu =>
+      run_instructions(cpu, 4)
+      checkRegsInCPU(cpu, 2, 8.U)
+      checkRegsInCPU(cpu, 3, 0.U)
+      checkRegsInCPU(cpu, 4, 1.U)
+      checkRegsInCPU(cpu, 5, 1.U)
+    }
+  }
+
+  it should "not jalr" in {
+    //      addi x1, x0, 8
+    //      jalr x2, x1, 0
+    //      addi x3, x0, 1
+    //      addi x4, x0, 1
+    //      addi x5, x0, 1
+    //    -> x2=8,x3=1,x4=1,x5=1
+    load_instructions("jalr2.txt")
+    test(new CoreTop) { cpu =>
+      run_instructions(cpu, 5)
+      checkRegsInCPU(cpu, 2, 8.U)
+      checkRegsInCPU(cpu, 3, 1.U)
+      checkRegsInCPU(cpu, 4, 1.U)
+      checkRegsInCPU(cpu, 5, 1.U)
+    }
+  }
+
+  //todo 这个不行
+  it should "jal" in {
+    //      jal x1, 8
+    //      addi x2, x0, 1
+    //      addi x3, x0, 1
+    //      addi x4, x0, 1
+    //    -> x1=4,x2=0,x3=1,x4=1
+    load_instructions("jal1.txt")
+    test(new CoreTop) { cpu =>
+      run_instructions(cpu, 3)
+      checkRegsInCPU(cpu, 1, 4.U)
+      checkRegsInCPU(cpu, 2, 0.U)
+      checkRegsInCPU(cpu, 3, 1.U)
+      checkRegsInCPU(cpu, 4, 1.U)
+    }
+  }
+
+  it should "lui" in {
+    //      lui x1, 0x12345
+    //      addi x2, x0, 1
+    //      addi x3, x0, 1
+    //      addi x4, x0, 1
+    //    -> x1=0x12345000,x2=1,x3=1,x4=1
+    load_instructions("lui1.txt")
+    test(new CoreTop) { cpu =>
+      run_instructions(cpu, 4)
+      checkRegsInCPU(cpu, 1, "h12345000".U)
+      checkRegsInCPU(cpu, 2, 1.U)
+      checkRegsInCPU(cpu, 3, 1.U)
+      checkRegsInCPU(cpu, 4, 1.U)
+    }
+  }
+
+  //todo 这里有问题，似乎x1取的是next pc的值
+  it should "auipc" in {
+    //      auipc x1, 0x12345
+    //      addi x2, x0, 1
+    //      addi x3, x0, 1
+    //      addi x4, x0, 1
+    //    -> x1=0x12345000,x2=1,x3=1,x4=1
+    load_instructions("auipc1.txt")
+    test(new CoreTop) { cpu =>
+      run_instructions(cpu, 4)
+      checkRegsInCPU(cpu, 1, "h12345000".U)
+      checkRegsInCPU(cpu, 2, 1.U)
+      checkRegsInCPU(cpu, 3, 1.U)
+      checkRegsInCPU(cpu, 4, 1.U)
+    }
+  }
 
 }
