cecd1b0257881aaf16a070476df28401c2a83cbf
redhecker <12210305@mail.sustech.edu.cn>
2024-05-24 01:12:03 +0800

    finish test2

--- software/test2.asm
+++ software/test2.asm
@@ -45,6 +45,8 @@ addi a5, a5, 1
 beq a5, t1, case7
 beq zero, zero, ini
 
+
+
 # Calculate the number of zeros
 case0:
 lw t0, (a1)
@@ -63,6 +65,8 @@ sw t2, (a0)
 sw t2, (a3)
 jal ini
 
+
+
 # Round up
 case1:
 lw t0, (a1)
@@ -75,33 +79,133 @@ andi t2, t2, 5
 addi t2, t2, -15 # exponent
 srli t3, t0, 15
 andi t3, t3, 1 # sign
-blt t2, zero, case1_end1
+blt t2, zero, case1_end1 # exp < 0
+# shift
+ori t1, t1, 1024
+addi t2, t2, -10
+blt t2, zero case1_small # exp < 10
+sll t4, t1, t2
+beq zero, zero, case1_end2
+
+case1_small: # exp < 10
+not t2, t2,
+addi t2, t2, 1
+srl t4, t1, t2 # round down
+not t5, t4
+addi t5, t5, 1
+add t5, t1, t5 # origin number - t4
+beq t3, zero, case1_pass
+not t4, t4
+addi t4, t4, 1
+case1_pass:
+beq zero, t5, case1_end2 # fraction part = 0
+addi t4, t4, 1
+
+case1_end2: # exp >= 0
+sw t4, (a0)
+sw t4, (a3)
+jal ini
+
+case1_end1: # exp < 0
+not t3, t3
+sw t3, (a0)
+sw t3, (a3)
+jal ini
+
 
 
 # Round down
 case2:
 lw t0, (a1)
 andi t0, t0, 4
-beq t0, zero, case1
+beq t0, zero, case2
 lw t0, (a2) # the original float-16 unmber
 andi t1, t0, 1023 # fraction
 srli t2, t0, 10
-andi t2, t2, 5 # exponent
+andi t2, t2, 5 
+addi t2, t2, -15 # exponent
 srli t3, t0, 15
 andi t3, t3, 1 # sign
+blt t2, zero, case2_end1 # exp < 0
+# shift
+ori t1, t1, 1024
+addi t2, t2, -10
+blt t2, zero case2_small # exp < 10
+sll t4, t1, t2
+beq zero, zero, case2_end2
+
+case2_small: # exp < 10
+not t2, t2,
+addi t2, t2, 1
+srl t4, t1, t2 # round down
+beq t3, zero, case2_end2
+not t4, t4
+addi t4, t4, 1
+
+case2_end2: # exp >= 0
+sw t4, (a0)
+sw t4, (a3)
+jal ini
+
+case2_end1: # exp < 0
+not t3, t3
+addi t3, t3, -1
+sw t3, (a0)
+sw t3, (a3)
+jal ini
+
+
 
 
 # Round to the nearest whole number
 case3:
 lw t0, (a1)
 andi t0, t0, 4
-beq t0, zero, case1
+beq t0, zero, case3
 lw t0, (a2) # the original float-16 unmber
 andi t1, t0, 1023 # fraction
 srli t2, t0, 10
-andi t2, t2, 5 # exponent
+andi t2, t2, 5 
+addi t2, t2, -15 # exponent
 srli t3, t0, 15
 andi t3, t3, 1 # sign
+blt t2, zero, case3_end1 # exp < 0
+# shift
+ori t1, t1, 1024
+addi t2, t2, -10
+blt t2, zero case3_small # exp < 10
+sll t4, t1, t2
+beq zero, zero, case3_end2
+
+case3_small: # exp < 10
+not t2, t2,
+addi t2, t2, 1
+srl t4, t1, t2 # round down
+not t5, t4
+addi t5, t5, 1
+add t5, t1, t5 # origin number - t4
+slli t5, t5, 1
+srl t5, t5, t2 # t5 = fraction part >= 0.5 ? 1 : 0
+beq t3, zero, case3_pass
+not t4, t4
+addi t4, t4, 1
+case1_pass:
+beq zero, t5, case3_end2 # fraction part < 0.5
+addi t4, t4, 1
+
+case3_end2: # exp >= 0
+sw t4, (a0)
+sw t4, (a3)
+jal ini
+
+case3_end1: # exp < 0
+not t3, t3
+sw t3, (a0)
+sw t3, (a3)
+jal ini
+
+
+
 
 
 # Add two numbers
@@ -131,6 +235,9 @@ sw t3, (a0)
 sw t3, (a3)
 jal ini
 
+
+
+
 # little-endian -> big endian
 case5:
 lw t0, (a1)
@@ -145,7 +252,7 @@ slli t2, t2, 4
 srli t3, t3, 12
 or t0, t2, t3
 sw t0, (a0)
-sw, (a3)
+sw t0, (a3)
 jal ini
 
 
